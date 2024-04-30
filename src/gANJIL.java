5e98e02ef7e6f278ad9068ddf5c326886670c6ea
ghilmanzikra <ghilmanzikraoke@gmail,com>
2024-04-30 10:07:12 +0700

    oke

---
+++ src/gANJIL.java
@@ -0,0 +1,14 @@
+
+
+public class gANJIL {
+    public static void main(String[] args) {
+        int[] arr = {7,12,16,15,3,6};
+        for (int i : arr){
+            if (i % 2 != 0){
+                System.out.println(i + " is odd");
+            }else {
+                System.out.println(i + " is even");
+            }
+        }
+    }
+}
